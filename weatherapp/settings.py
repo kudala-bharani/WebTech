396abeabd78403d3c6beff3f4aa2775d95285868
Kudala Bharani Kumar Reddy <bunnykudala@gmail.com>
2024-12-02 22:07:33 +0530

    Update settings.py

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT=BASE_DIR/'assets'
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
